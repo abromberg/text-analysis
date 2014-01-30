c063343f60c2d599550ee743070075dae8636219
Andy Bromberg <abromberg@stanford.edu>
2014-01-30 08:56:46 -0800

    cleaned up createTermSentences, got ri dof load from text file

--- createTermSentences.py
+++ createTermSentences.py
@@ -6,24 +6,8 @@ Use the following function to get all sentences containing a certain term from a
 Requires a STRING for desiredTerm and a FILE LOCATION for path
 """
 
-def createTermSentences(desiredTerm, path):
-
-	"""opens text output file"""
-	f = open(path,"r")
-
-	"""creates string with text from file"""
-	text = f.read()
-
-	"""creates NLTK's tokenizer"""
+def createTermSentences(desiredTerm, text):
 	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
-	"""Uses tokenizer to separate text into sentences"""
 	sentences = tokenizer.tokenize(text)
-
-	"""Desired term in sentences"""
-	keyTerm = desiredTerm
-
-	"""Creates array of sentences containing keyTerm"""
-	termSentences = [sentence for sentence in sentences if keyTerm in sentence]
-
-	print termSentences
+	termSentences = [sentence for sentence in sentences if desiredTerm in sentence]
+        return termSentences
